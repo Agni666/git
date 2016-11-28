bdc4ddd62c9afbff6f77576eb5b272ce8cd21000
lukasz <lukasz.widuch@smcebi.edu.pl>
2016-11-28 10:03:33 +0000

    zrobic strip z kazdej lini i dodac do listy appendem

--- wyrazy.py
+++ wyrazy.py
@@ -1,13 +1,7 @@
-                                                                                                                          
-text = open('tekst.txt').read() 
-text.split(' ',) 
-print text
-                                                                                                                                          
-lista=[] for j in range (30):
-        lista.append(0)
-                                                                                                                                          
-for i in text:
-        lista[len(i)]+=1
-                                                                                                                                          
-print lista
-                
+def pobierz_wyrazy(nazwa):
+    plik = open(nazwa,'r')
+    for wyraz in plik.readlines():
+        print(wyraz)
+    plik.close()
+        
+pobierz_wyrazy('wyrazy.txt')
