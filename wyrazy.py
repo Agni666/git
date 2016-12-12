e58a84508bb0ae39138cef2471d13aead4920085
Grzegorz Klauza <grzegorz.klauza@alfa1.smcebi.us.edu.pl>
2016-12-12 09:46:18 +0000

    usuwa znaki interpunkcyjne

--- wyrazy.py
+++ wyrazy.py
@@ -6,6 +6,7 @@ def pobierz_wyrazy(nazwa):
     lista = []
     plik = open(nazwa,'r')
     calosc = plik.read()
+    calosc = calosc.remove(".",",",";","?" )
     calosc = calosc.split(" ")
     for wyraz in calosc:
         lista.append(wyraz)
